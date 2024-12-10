4df674c7b96cea34f40af715af6972ae4cc14308
neorack <cumin_murky.0a@icloud.com>
2024-12-10 08:39:10 -0700

    feat: Add Aspire hosting to Neoron.AppHost

--- Source/Neoron.AppHost/Program.cs
+++ Source/Neoron.AppHost/Program.cs
@@ -57,3 +57,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.Neoron_API>("api");
 
 builder.Build().Run();
+using Aspire.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.Neoron_API>("api");
+
+await builder.Build().RunAsync();
