71cca97d25d8d7fb6d3564259c87a6956687027d
neorack <cumin_murky.0a@icloud.com>
2024-12-10 08:38:55 -0700

    fix: Remove unnecessary DiscordMessage index

--- Source/Neoron.Domain/Indexes/IX_DiscordMessages_GuildChannel.sql
+++
@@ -1,5 +0,0 @@
-CREATE NONCLUSTERED INDEX [IX_DiscordMessage_GuildChannel]
-ON [dbo].[DiscordMessage] ([GuildId], [ChannelId])
-INCLUDE ([MessageId], [AuthorId], [Content], [CreatedAt])
-WHERE IsDeleted = 0;
-GO
